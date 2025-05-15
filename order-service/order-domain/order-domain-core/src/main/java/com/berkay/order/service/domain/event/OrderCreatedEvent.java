27445445c253111ec45c954cec9163613d988610
OzerBerkay <berkay1690@gmail.com>
2025-05-15 16:24:23 +0300

    Order events and processes implemented

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/berkay/order/service/domain/event/OrderCreatedEvent.java
@@ -0,0 +1,12 @@
+package com.berkay.order.service.domain.event;
+
+import com.berkay.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCreatedEvent extends OrderEvent {
+
+    public OrderCreatedEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
