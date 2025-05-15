19bc3f3df7d7dbc30c39534cc2ac9b17a47cf6dd
OzerBerkay <berkay1690@gmail.com>
2025-05-15 14:25:39 +0300

    Customer and Restaurant aggregate roots added

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/berkay/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.berkay.order.service.domain.entity;
+
+import com.berkay.domain.entity.AggregateRoot;
+import com.berkay.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
