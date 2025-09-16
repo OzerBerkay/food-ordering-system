e4d829716f7a5a8e0aecb70e37d75193966f29ea
berkaycodefirst <berkay.ozer@codefirst.io>
2025-09-16 05:46:18 +0300

    Tests are added for order application service

--- order-service/order-domain/order-application-service/src/main/java/com/berkay/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/berkay/order/service/domain/OrderCreateCommandHandler.java
@@ -2,22 +2,11 @@ package com.berkay.order.service.domain;
 
 import com.berkay.order.service.domain.dto.create.CreateOrderCommand;
 import com.berkay.order.service.domain.dto.create.CreateOrderResponse;
-import com.berkay.order.service.domain.entity.Customer;
-import com.berkay.order.service.domain.entity.Order;
-import com.berkay.order.service.domain.entity.Restaurant;
 import com.berkay.order.service.domain.event.OrderCreatedEvent;
-import com.berkay.order.service.domain.exception.OrderDomainException;
 import com.berkay.order.service.domain.mapper.OrderDataMapper;
 import com.berkay.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
-import com.berkay.order.service.domain.ports.output.repository.CustomerRepository;
-import com.berkay.order.service.domain.ports.output.repository.OrderRepository;
-import com.berkay.order.service.domain.ports.output.repository.RestaurantRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-import java.util.UUID;
 
 @Slf4j
 @Component
@@ -41,6 +30,6 @@ public class OrderCreateCommandHandler {
         OrderCreatedEvent orderCreatedEvent = orderCreateHelper.persistOrder(createOrderCommand);
         log.info("Order is created with id: {}", orderCreatedEvent.getOrder().getId().getValue());
         orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
-        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder());
+        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder(), "Order Created Successfully");
     }
 }
